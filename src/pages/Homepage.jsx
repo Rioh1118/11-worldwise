130f25539c19fbbf7f8eb8250046a22fe62e684f
Rioh1118 <rio2515t@gmail.com>
2023-11-21 15:41:01 +0900

    Link and Nav

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-  return <div>under</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>World wise!</h1>
+
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default Homepage;
